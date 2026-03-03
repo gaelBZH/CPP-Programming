d24666869fedd170512a452ebd8daffdbf28a83b
gaelBZH <gaelkerninon3110@gmail.com>
2026-03-03 16:35:30 +0100

    Calculator

---
+++ E-learning Course/Calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// This is my first
+
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout << "a = ";
+    cin >> a;
+
+    cout << "b = ";
+    cin >> b;
+
+    int sum = a + b;
+    cout << "The sum of a and b is " << sum << ".\n";
+    
+    int sub = a - b;
+    cout << "The substraction of a and b is " << sub << ".\n";
+
+    int prod = a * b;
+    cout << "The product of a and b is " << prod << ".\n";
+
+    if (b == 0)
+    {
+        cout << "You can't divide by 0 !";
+    }
+    else
+    {
+        int div = a / b;
+        cout << "The division of a and b is " << div << ".\n";
+    }
+    
+    return 0;
+}
