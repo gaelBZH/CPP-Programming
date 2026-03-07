b4fb61fbfae1376d2ef9dd1d126e91a26de5f72f
gaelBZH <gaelkerninon3110@gmail.com>
2026-03-07 18:37:48 +0100

    Exercice 5.1 new version

--- E-learning Course/5.1-Function.cpp
+++ E-learning Course/5.1-Function.cpp
@@ -20,32 +20,12 @@ int whichQuarter(float x, float y);
 int whichQuarter(float x, float y)
 {
     if (x == 0 || y == 0)
-    {
         return 0; // On a line
-    }
 
     if (x < 0)
-    {
-        if (y < 0)
-        {
-            return 3;
-        }
-        else
-        {
-            return 2;
-        }
-    }
+        return (y < 0) ? 3 : 2;
     else
-    {
-        if (y < 0)
-        {
-            return 4;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+        return (y < 0) ? 4 : 1;
 }
 
 int main()
